7af289c9dd3b95e5b7b09349ff9e22ae5affb1cc
unknown <emanuele.zini11@gmail.com>
2022-05-10 20:20:25 +0200

    feat: added modules and create the navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CoinList from "./pages/CoinList";
+import WalletData from "./pages/WalletData";
+
+const axios = require("axios");
 
 function App() {
+  const [coins, setCoins] = useState("");
+  const [coinSelected, setCoinSelected] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000")
+      .then((response) => {
+        setCoins(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Defi Tracker</header>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<WalletData />} />
+          <Route path="/coinlist" exact element={<CoinList />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
